38121d2101028961c1a7e6a95c454ddb7cb3500e
Evan <53783383+EvanPole@users.noreply.github.com>
2023-06-29 14:56:23 +0200

    add calc function

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 ///////////////
 
 let buffer = "";
-let calc = 0;
+let calc = "";
+let waitcalc = "";
 let calcbox = document.getElementById("calcbox");
 
 //Settings
@@ -11,27 +12,45 @@ calcbox.value = "0";
 
 
 //function
-function setcalc(value) {
 
-}
-
-function getcalc(value) {
-
-}
+function calculerExpression(expression) {
+    expression = expression.replace(/\s/g, '');
+  
+    var caracteresValides = /^[0-9+\-*/^().]+$/;
+    if (!caracteresValides.test(expression)) {
+      return 'Expression invalide';
+    }
+  
+    try {
+      var resultat = eval(expression); 
+      return resultat;
+    } catch (error) {
+      return 'Erreur lors de l\'évaluation de l\'expression';
+    }
+  }
 
 function addcalc(input) {
+
     if (input == 'c') {
         calcbox.value = "";
         buffer = "";
         calc = "";
         input = "";
     }
+    if (input == '=') {
+        console.log(calculerExpression(buffer));
+        calcbox.value = calculerExpression(buffer);
+        buffer = calculerExpression(buffer)
+        input = "";
+    }
     buffer += input;
-    // console.log(buffer);
     calcbox.value = buffer;
+
 }
 
 
+
+  
 // setTimeout(() => {
 //     console.log(info);
 //     document.getElementById("calcbox").value = "hoho"
